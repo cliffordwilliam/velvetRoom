65ef05194d7ad65ae0d84ae8b7df1bbbfdea8513
Clifford William <ccliffordwilliam@gmail.com>
2023-12-08 18:00:16 +0800

    Fix missing componenets

---
+++ testAuth/src/components/Decorations.jsx
@@ -0,0 +1,25 @@
+function Decorations() {
+    return (
+        <>
+            {/* START DECORATIONS */}
+            {/* circle texture */}
+            <div className="circle-texture"></div>
+            {/* circle texture big */}
+            <div className="circle-texture-big"></div>
+            {/* circle texture bottom */}
+            <div className="circle-texture-bottom"></div>
+            {/* circle texture big bottom */}
+            <div className="circle-texture-big-bottom"></div>
+            {/* lines texture */}
+            <div className="lines-texture"></div>
+            {/* grid texture */}
+            <div className="grid-texture"></div>
+            {/* vignette */}
+            <div className="vignette"></div>
+            {/* END DECORATIONS */}
+        </>
+    )
+}
+
+
+export default Decorations
---
+++ testAuth/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+function Hero() {
+    return (
+        <>
+            <section className="hero">
+                <div className="con">    
+                    <h1 className="hero-title">Rent Rooms | Hacktiv</h1>
+                    <p className="hero-sub-title">Best lodging in town</p>
+                </div>
+                <img className="hero-img" src="./src/assets/b.webp" alt="Hero Image" />
+            </section>
+        </>
+    )
+}
+
+
+export default Hero
